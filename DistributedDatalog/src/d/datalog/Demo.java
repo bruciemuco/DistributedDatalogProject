b0330eee2cb66785333cda8fb4ae138de8a46341
Brice Muco <mucobrice@gmail.com>
2016-10-16 18:13:08 -0400

    working for nondeterministic goals (multiple solutions)

--- DistributedDatalog/src/d/datalog/Demo.java
+++ DistributedDatalog/src/d/datalog/Demo.java
@@ -3,27 +3,27 @@ package d.datalog;
 import java.io.File;
 
 import com.declarativa.interprolog.*;
-
+//refer to: https://github.com/sepehram/retro-security-openmrs/blob/master/retrosecurity/api/src/main/java/com/declarativa/interprolog/examples/HelloWorld.java
 public class Demo {
 
-	public static void main(String[] args){
-		
-		XSBSubprocessEngine engine = new XSBSubprocessEngine("D:\\XSB\\bin\\xsb64.bat");
-		if (engine.deterministicGoal("javaMessage('java.lang.System'-out,println(string('Hello from Prolog, Java world!')))"))
+	public static void main(String[] args) {
+
+		XSBSubprocessEngine engine = new XSBSubprocessEngine(
+				"D:\\XSB\\bin\\xsb64.bat");
+		if (engine
+				.deterministicGoal("javaMessage('java.lang.System'-out,println(string('Hello from Prolog, Java world!')))"))
 			System.out.println("This goal succeeded");
 		File f = new File("D:\\XSB\\prolog-commons\\p.P");
-		engine.consultAbsolute(f );
-		String RVars = null;
+		engine.consultAbsolute(f);
+		String RVars = "[TM]";
 		Object[] c = null;
-		c = engine.deterministicGoal("p(1,X)", RVars);
-		System.out.println(c[0]);
-		SolutionIterator e = engine.goal("p(1,X)", RVars);
-		String x = e.toString();
-		
-		
+		SolutionIterator e = engine.goal("p(1,X),buildTermModel(X,TM)", "[TM]");
+		while (e.hasNext()) {
+			c = e.next();
+			System.out.println(c[0]);
+		}
+
 		engine.shutdown();
-		
-		
-		
+
 	}
 }
